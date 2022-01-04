47007f04f43b8673cfde0e1f0b14b28156b13328
Alec Chen <alecchendev@gmail.com>
2022-01-03 20:10:49 -0600

    Comment and clean Multisig.sol

--- src/Multisig.sol
+++ src/Multisig.sol
@@ -1,16 +1,21 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.11;
 
-
+/// @title Multisig wallet
+/// @author Illini Blockchain
+/// @notice A simple implementation of a multisig wallet
 contract Multisig {
 
-    event CreateTx(address to, uint value, bytes data);
+    /// EVENTS
+    /// Use events to log data about interaction with the multisig - consumable by clients
+    event CreateTx(address to, uint value, bytes data, bytes32 txHash, bytes32 pendingHash);
     event SignTx(bytes32 pendingHash, address signer);
     event UnsignTx(bytes32 pendingHash, address signer);
     event SendTx(bytes32 txHash);
     event AddOwner(address newOwner);
     event RemoveOwner(address owner);
 
+    /// STRUCTS
     struct Transaction {
         address to;
         uint value;
@@ -24,64 +29,89 @@ contract Multisig {
         mapping (address => bool) signers;
     }
 
+    /// CONTRACT STATE
     mapping (address => bool) public owners;
     mapping (bytes32 => Transaction) public txs;
     mapping (bytes32 => PendingTx) public pending;
 
+    /// MODIFIERS
+    /// Functions with this modifier can only be called by an owner
+    modifier onlyOwner {
+        require(owners[msg.sender]);
+        _;
+    }
+
+    /** To add or remove an owner, we may want multiple people to sign off
+        similar to another multisig tx. We can make those functions public
+        and use this modifier to allow us to use our built-in multisig
+        functionality to extend to these functions.
+     */
+    modifier onlyContract {
+        require(msg.sender == address(this));
+        _;
+    }
+
+    /// FUNCTIONS
+    /// @notice Initialize multisig with initial owners
+    /// @param initialOwners List of addresses of owners
     constructor (address[] memory initialOwners) {
         for (uint i = 0; i < initialOwners.length; i += 1) {
             owners[initialOwners[i]] = true;
         }
     }
 
-    modifier onlyOwner {
-        require(owners[msg.sender]);
-        _;
-    }
-
-    // internal functions that need multisig sign off
-    modifier onlyContract {
-        require(msg.sender == address(this));
-        _;
-    }
-
-    /// @notice Adds an owner
+    /// @notice Adds a new owner
+    /// @param newOwner Address of owner to add
     function addOwner(address newOwner) public onlyContract {
         // Add an owner
         // Log event
     }
 
-    /// @notice Removes current owner
+    /// @notice Removes existing owner
+    /// @param owner Address of existing owner to remove
     function removeOwner(address owner) public onlyContract {
         // Remove owner
         // Log event
     }
 
+    /// @notice AddOwner and removeOwner in one transaction
+    /// @param currOwner Address of owner to remove
+    /// @param newOwner Address of owner to add
     function changeOwner(address currOwner, address newOwner) public onlyContract {
         removeOwner(currOwner);
         addOwner(newOwner);
     }
 
-    /// @notice Initialize a transaction and be the first signer
-    function createTx(address to, uint value, bytes memory data) public onlyOwner returns (bytes32) {
+    /// @notice Initialize a transaction and adds first signature
+    /// @dev Create tx and pending hash using keccak256(abi.encodePacked(...))
+    /// @param to Address to send transaction to
+    /// @param value Amount in wei (eth/1e18) to send
+    /// @param data Transaction data
+    function createTx(address to, uint value, bytes memory data) public onlyOwner {
         // create Transaction
         // create PendingTx
         // signTx
         // log event
     }
 
-    /// @notice Sign off on a transaction and execute it if enough signatures
+    /// @notice Signs off on a transaction and execute it if enough signatures
+    /// @param pendingHash Hash that maps to the PendingTx to unsign 
     function signTx(bytes32 pendingHash) public onlyOwner {
         // sign tx
         // log event
+        // sendTx if enough sigs
     }
 
+    /// @notice Removes existing signature from a PendingTx
+    /// @dev Check if they're signature exists to properly update n_signed
+    /// @param pendingHash Hash that maps to the PendingTx to unsign
     function unsignTx(bytes32 pendingHash) public onlyOwner {
         // remove signature from tx
+        // log event
     }
 
     /// @notice Wrapper to send transaction once approved
-    /// @param txHash hash that maps to the tx
+    /// @param txHash Hash that maps to the Transaction
     function sendTx(bytes32 txHash) private {
         // get transaction data from txHash map
         // call transaction
@@ -89,4 +119,3 @@ contract Multisig {
     }
 
 }
-
